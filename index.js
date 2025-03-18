8fcab7b6f532a8d871d32b068a325fccf3d0abf1
umesh raut <uraut6044@gmail.com>
2025-03-18 23:43:07 +0530

    type module added in package json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-import e from "express";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+
+}));
+
+app.use(express.json());
+
+const port = process.env.PORT || 4000
+
+app.get("/", (req, res) => {
+    res.send("FIne");
+});
+
+app.listen(port, () => {
+    console.log(`App Listening on port : ${port}`);
+})
